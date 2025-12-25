413bbb741fe4dd6565e987dc8a24b10705ec6f1d
und3rr00t <ousamaeddahbir@gmail.com>
2025-12-25 13:45:04 +0000

    upgrade shell tester

--- Butterfly_algo/test.sh
+++ Butterfly_algo/test.sh
@@ -1,17 +1,46 @@
-for i in {1..100}; do 
-    ARG=$(shuf -i 1-500 | tr '\n' ' '); 
-    ./push_swap $ARG | wc -l >> results.txt; 
-    ./push_swap $ARG | ./checker_linux $ARG >> checks.txt;
-done
+ITERATIONS=50
+INT_MIN=-2147483648
+INT_MAX=2147483647
 
-echo "--- RESULTS ---"
-echo "Average Moves:"
-awk '{ total += $1 } END { print total/NR }' results.txt
-echo "Max Moves:"
-sort -nr results.txt | head -1
-echo "Min Moves:"
-sort -n results.txt | head -1
-echo "Errors (Should be 0):"
-grep -v "OK" checks.txt | wc -l
+run_test() {
+    local COUNT=$1
+    local TEST_NAME=$2
+    
+    echo "=================================================="
+    echo "  RUNNING $TEST_NAME TEST ($ITERATIONS iterations)"
+    echo "=================================================="
 
-rm results.txt checks.txt
+    > results.txt
+    > checks.txt
+
+    for i in $(seq 1 $ITERATIONS); do
+        echo -ne "Progress: $i / $ITERATIONS\r"
+        ARG=$(python3 -c "import random; print(' '.join(map(str, random.sample(range($INT_MIN, $INT_MAX), $COUNT))))")
+        ./push_swap $ARG | wc -l >> results.txt
+        ./push_swap $ARG | ./checker_linux $ARG >> checks.txt
+    done
+
+    echo -e "\n--- $TEST_NAME STATISTICS ---"
+
+    AVG=$(awk '{ total += $1 } END { printf "%.0f", total/NR }' results.txt)
+    echo "Average Moves: $AVG"
+
+    MAX=$(sort -nr results.txt | head -1)
+    echo "Max Moves:     $MAX"
+
+    MIN=$(sort -n results.txt | head -1)
+    echo "Min Moves:     $MIN"
+
+    ERRORS=$(grep -v "OK" checks.txt | wc -l)
+    if [ "$ERRORS" -eq 0 ]; then
+        echo "Validity:      PASS (All OK)"
+    else
+        echo "Validity:      FAIL ($ERRORS errors found!)"
+    fi
+    echo ""
+    rm results.txt checks.txt
+}
+
+run_test 100 "STACK 100"
+
+run_test 500 "STACK 500"
